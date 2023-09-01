6dba265e493a7d6311ee13b380935cd65768c8b4
kateleighb3 <kbaldw3@gmail.com>
2023-08-31 20:44:25 -0400

    bg image cover

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -11,7 +11,7 @@ const Sidenav = () => {
     };
 
     return (
-        <div className='fixed w-full h-screen bg-[url("./assets/cole-patrick-hpCHLFknc2s-unsplash.jpg")] flex flex-col justify-center items-center z-20'>
+        <div className='fixed w-full h-screen bg-cover bg-[url("./assets/cole-patrick-hpCHLFknc2s-unsplash.jpg")] flex flex-col justify-center items-center z-20'>
             <AiOutlineMenu onClick={handleNav} className = 'absolute top-4 right-4 z-[99] md:hidden'/>
             {
                 //if our nav state is true- then display html on the screen and if it's not true, we will display some differnt html on the screen
