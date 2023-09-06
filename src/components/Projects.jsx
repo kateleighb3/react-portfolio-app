5136e02a6ed015c00474e7dccc4d92198e0d8abf
kateleighb3 <kbaldw3@gmail.com>
2023-09-05 21:51:54 -0400

    add links to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,9 +8,8 @@ const Projects = () => {
         <div id='projects' className='w-full md:pl-20 p-4 bg-rose-900 py-16'>
              <h1 className='text-4xl font-bold text-center font-fascinate mb-8'>Projects</h1>
             <div className='grid sm:grid-cols-2 gap-12'>
-                <ProjectItem img={ironman} title='The Comic Vault'/>
-                <ProjectItem img={quest} title='An App Called Quest'/>
-
+                <a href='https://sienkc.github.io/The_Comic_Vault/'><ProjectItem img={ironman} title='The Comic Vault' tech='HTML/CSS/Javascript'/></a>
+                <a href='https://shrouded-springs-93967-4ef63984c8fc.herokuapp.com/'><ProjectItem img={quest} title='An App Called Quest' tech='Node/MySQL' a href='https://sienkc.github.io/The_Comic_Vault/'/></a>
             </div>
 
         </div>
